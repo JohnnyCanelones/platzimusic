8e0a57866620d84015d81b2c3e7eec23c994068c
johnnyjose13@gmail.com <johnnyjose13@gmail.com>
2019-02-10 11:57:30 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    h2 Pais
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" :value="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul 
+      artist( v-for="artist in artists" :artist="artist" :key="artist.mbid" ) 
+</template>
+
+<script>
+import getArtist from './api'
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      countries: [
+        {name: 'Venezuela', value: 'venezuela'},
+        {name: 'Colombia', value: 'colombia'},
+        {name: 'España', value: 'spain'},
+      ],
+      selectedCountry: 'venezuela',
+      loading: true,
+    }
+  },
+  methods: {
+    refreshArtist(country) {
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtist(country)
+        .then(function (artists){
+          self.loading= false
+          self.artists = artists
+        })  
+    }
+  },
+  mounted () {    
+    this.refreshArtist(this.selectedCountry)
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtist(this.selectedCountry)
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+}
+</script>
+
+<style lang="stylus">
+  #app 
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50 
+    margin-top 60px
+  
+  h1, h2 
+    font-weight normal
+  
+  ul 
+    list-style-type none
+    padding 0
+  li 
+    display inline-block
+    margin 0 10px
+  a 
+    color #42b983
+
+    
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: '3a8eb17720ce6faae1dc7cf258fc5c4f',
+    secret: '35961d7da44fdbeecb03dcc73231f3d2',
+    registeredTo: 'johnnycanelones',
+}
